c0b156bad9c47ae5407263f94541a20eb918eb61
Zakaria Boudribila <zakariaboudribila1@gmail.com>
2026-03-11 00:20:47 +0000

    Fix: URL API correcte du serveur

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // UTILISE CETTE URL (sans le :3001)
-  apiBaseUrl: 'https://c7a52bc1-3ed6-428a-8138-d3274520eeeb-00-pvly0co2xfg7.universal.replit.dev/api'
+  apiBaseUrl: 'https://app-y--zakariabd.replit.app'
 };
